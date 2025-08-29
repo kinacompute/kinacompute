89ab0aa6c505ed061cb772722a3cfb4a8667adc2
ternicinc <allexander.linux@gmail.com>
2025-08-29 12:51:56 +0200

    CONF: Mandatory fields.

--- config.rb
+++ config.rb
@@ -1,3 +1,5 @@
+require_relative "lib/config_helpers"
+
 module Config
 	extend ConfigHelpers
 
@@ -12,3 +14,8 @@ module Config
 	def self.test?
 		Config.rack_env == "test"
 	end
+
+	# Mandatory -- An exception is raied for those values when missing.
+	# This snippet is based on the UbiCloud config.rb.
+	mandatory :database_url, string, clear: true
+	mandatory :rack_env, string
